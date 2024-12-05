7238eedb05869398d48f0916bb84e058a044f438
Hugo Morvan <Hugo.Morvan@coyotes.usd.edu>
2024-12-05 13:31:29 +0100

    commented

--- day5/day5_1.py
+++ day5/day5_1.py
@@ -6,9 +6,8 @@ def all_increasing(idx):
         prev = e
     return True
 
-my_set = []
-dep = []
-books = []
+dep = []    # list of X|Y
+books = []  # list of {a,b,c,...}
 
 with open("input.txt", 'r') as text:
     lines = text.readlines()
@@ -17,16 +16,15 @@ with open("input.txt", 'r') as text:
         if len(line) == 6: dep.append(line)
         else: books.append(line)
 
-for line in dep:
-    a,b = map(int ,line.split('|'))
-    my_set.append(a)
-    my_set.append(b)
-       
-uniques = list(set(my_set))
-print(f"{uniques=}")
-
 
 def build_dep(dep, book):
+    """
+    Given the dependency rules and a book ({a,b,c,...})
+    returns a list containing the number of times a page in a book is dependent on another page
+    Example: 
+    dep = C|A, C|B, B|A and book = {A,B,C}
+    returns [2,1,0]
+    """
     dependencies = [0]*len(book)
     for line in dep:
         a,b = map(int ,line.split('|'))
@@ -34,21 +32,22 @@ def build_dep(dep, book):
             col = book.index(b)
             dependencies[col] += 1
         else: continue
-    print(f"{dependencies=}")
+    #print(f"{dependencies=}")
     return dependencies
 
-correct_total = 0
-incorrect_total = 0
+correct_total = 0   # part 1
+incorrect_total = 0 # part 2
+
 for line in books:
     if len(line) != 6 and len(line) != 1:
-        print(line)
         book = list(map(int ,line.split(',')))
         dependencies = build_dep(dep, book)
         #print(dep_order)
         if all_increasing(dependencies):
             mid = len(book)//2
             correct_total += book[mid]
-        else:incorrect_total += book[dependencies.index(len(book)//2)]
+        else:
+            incorrect_total += book[dependencies.index(len(book)//2)]
     else: continue
-print(correct_total)
-print(incorrect_total)
+print(f"part 1: {correct_total}")
+print(f"part 2: {incorrect_total}")
